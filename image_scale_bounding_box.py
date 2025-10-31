43252a9c68051e506c4f88d53d149cd6485fea16
AlexL <github@leual.ch>
2025-10-31 18:28:47 +0100

    code refactor, type annotations

--- image_scale_bounding_box.py
+++ image_scale_bounding_box.py
@@ -20,51 +20,58 @@ class ImageScaleBoundingBox:
     CATEGORY = "Hangover"
 
     @classmethod
-    def INPUT_TYPES(s):
+    def INPUT_TYPES(cls):
         return {"required": {
                     "image": ("IMAGE",),
-                    "upscale_method": (s.UPSCALE_METHOD,),
-                    "box_width": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 1}),
-                    "box_height": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 1}),
-                    "padding": (["none"] + s.PADDING,),
-                    "pad_color": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFF, "step": 1, "display": "pad_color"}),
+                    "upscale_method": (cls.UPSCALE_METHOD,),
+                    "box_width": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 1},),
+                    "box_height": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 1},),
+                    "padding": (["none"] + cls.PADDING,),
+                    "pad_color": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFF, "step": 1, "display": "pad_color"},),
                     }
                 }
 
-    def upscale(self, image, upscale_method, box_width, box_height, padding, pad_color):
+
+    def upscale(self, image: torch.Tensor, upscale_method: str, box_width: int, box_height: int, padding: str, pad_color: int) -> tuple[torch.Tensor]:
         w = image.shape[2]
         h = image.shape[1]
         scale_by = min(box_width/w, box_height/h)
-        new_width = round(w * scale_by)
-        new_height = round(h * scale_by)
-        samples = comfy.utils.common_upscale(image.movedim(-1,1), new_width, new_height, upscale_method, "disabled").movedim(1,0)
-        if padding in ImageScaleBoundingBox.PADDING:
-            # if padding == "center":
+        new_width = round(number=w * scale_by)
+        new_height = round(number=h * scale_by)
+        samples = comfy.utils.common_upscale(
+            samples=image.movedim(source=-1, destination=1), 
+            width=new_width, height=new_height, 
+            upscale_method=upscale_method, crop="disabled"
+            ).movedim(source=1, destination=0)
+
+        if padding in self.PADDING:
+            # padding for the case 'center':
             pad_left = (box_width - new_width) // 2
             pad_right = box_width - new_width - pad_left
             pad_top = (box_height - new_height) // 2
             pad_bottom = box_height - new_height - pad_top # ensure that we do not get any rounding error in the output image size
-            if padding == "top":
-                pad_bottom = 0
-                pad_top = box_height - new_height
-            elif padding == "left":
-                pad_right = 0
-                pad_left = box_width - new_width
-            elif padding == "right":
-                pad_left = 0
-                pad_right = box_width - new_width
-            elif padding == "bottom":
-                pad_top = 0
-                pad_bottom = box_height - new_height
+ 
+            # override padding for the cases 'top', 'left', 'right', and 'bottom':
+            match padding:
+                case "top":
+                    pad_bottom = 0
+                    pad_top = box_height - new_height
+                case "left":
+                    pad_right = 0
+                    pad_left = box_width - new_width
+                case "right":
+                    pad_left = 0
+                    pad_right = box_width - new_width
+                case "bottom":
+                    pad_top = 0
+                    pad_bottom = box_height - new_height
+
             pad = (pad_left, pad_right, pad_top, pad_bottom)
+            b, g, r = [((pad_color >> (i * 8)) & 0xFF) / 255. for i in range(3)] # extract rgb values from pad_color
+            samples = torch.stack(
+                        tensors=[F.pad(input=samples[i], 
+                        pad=pad, mode='constant', value=value
+                        ) for i, value in enumerate(iterable=(r, g, b))])
 
-            r = ((pad_color & 0xFF0000) >> 16) / 255. # I know this & is not necessary, it's for readability
-            g = ((pad_color & 0x00FF00) >> 8)  / 255.
-            b = (pad_color & 0x0000FF) / 255.
-            ir = F.pad(input=samples[0], pad=pad, mode='constant', value = r)
-            ig = F.pad(input=samples[1], pad=pad, mode='constant', value = g)
-            ib = F.pad(input=samples[2], pad=pad, mode='constant', value = b)
-            samples = torch.stack([ir,ig,ib])
-
-        samples = samples.movedim(0,-1)
+        samples = samples.movedim(source=0, destination=-1) # restore the original shape of the image
         return (samples,)
