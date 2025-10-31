72c03a45918005612be1fa2a02267916f5f31117
AlexL <github@leual.ch>
2025-10-31 18:32:02 +0100

    remove redundant expression input field, refactor test code

--- math_interpreter.py
+++ math_interpreter.py
@@ -4,10 +4,10 @@
 @nickname: Hangover-Sympy_Interpreter
 @description: A mathematic expression interpreter based on the sympy library
 """
-from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 from comfy.comfy_types import IO
 import math
+from typing import Any
 
 class Sympy_Interpreter:
     RETURN_TYPES = (
@@ -28,22 +28,25 @@ class Sympy_Interpreter:
     FUNCTION = "calc"
     CATEGORY = "Hangover"
 
-    Input_Vars = [chr(c + ord("a")) for c in range(Config["num_vars"])]
-    Variables = {"expression": (IO.STRING, {"multiline": False, "default": "0"},)}
-    Variables.update({key: (IO.ANY,) for key in Input_Vars})
+    # Define the input variables dictionary:
+    Input_Vars: list[str] = [chr(c + ord("a")) for c in range(Config["num_vars"])]
+    Variables: dict[str, tuple] = {key: (IO.ANY,) for key in Input_Vars}
 
     @classmethod
-    def INPUT_TYPES(s):
+    def INPUT_TYPES(cls) -> dict[str, dict]:
         return {
             "required": {
                 "expression": (IO.STRING, {"multiline": False, "default": "0"},),
             },
-            "optional" : s.Variables,
+            "optional" : cls.Variables,
         }
 
-    def calc(self, expression:str, **kwargs):
+    
+    def calc(self, expression: str, **kwargs: Any) -> tuple[int, float, str]:
+        """ Evaluate the expression and return the results.  """
+        
         print(f"Math_Interpreter: evaluating expression A({expression})")
-        expr_A = parse_expr(expression, local_dict=kwargs)
+        expr_A = parse_expr(s=expression, local_dict=kwargs)
         try:
             result_A = float(expr_A)
         except TypeError:
@@ -55,26 +58,30 @@ class Sympy_Interpreter:
             str(expr_A),
         )
 
-    def selfTest(self):
-        from random import random
-        try:
-            print(f"{self.INPUT_TYPES()=}")
-            print(f"{self.RETURN_TYPES=}, {self.RETURN_NAMES=}")
-            print(f"{self.Input_Vars=}")
-            variables = {key: random() for key in self.Input_Vars}
-            print(f"{variables=}")
-            expression = f"{self.Input_Vars[0]}"
-            for v in self.Input_Vars[1:-1]:
-                expression += f"+{v}"
-            res_int, res_flt, res_str = self.calc(expression, **variables)
-            print(f"{res_int=}, {res_flt=}, {res_str=}")
-            print(f"{type(res_int)=}, {type(res_flt)=}, {type(res_str)=}")
-            print("Tests sucess")
-        except:
-            print("Tests failed")
-            raise
+
+def selfTest() -> None:
+    from random import random
+
+    sympy: Sympy_Interpreter = Sympy_Interpreter()
+    try:
+        print(f"{sympy.INPUT_TYPES()=}")
+        print(f"{sympy.RETURN_TYPES=}")
+        print(f"{sympy.RETURN_NAMES=}")
+        print(f"{sympy.Input_Vars=}")
+        print(f"{sympy.Variables=}")
+        variables = {key: random() for key in sympy.Input_Vars}
+        print(f"{variables=}")
+        expression = f"{sympy.Input_Vars[0]}"
+        for v in sympy.Input_Vars[1:-1]:
+            expression += f"+{v}"
+        res_int, res_flt, res_str = sympy.calc(expression=expression, **variables)
+        print(f"{res_int=}, {res_flt=}, {res_str=}")
+        print(f"{type(res_int)=}, {type(res_flt)=}, {type(res_str)=}")
+        print("Tests sucess")
+    except:
+        print("Tests failed")
+        raise
 
 
 if __name__ == "__main__":
-    sympy = Sympy_Interpreter()
-    sympy.selfTest()
+    selfTest()
