fa07cfb147c94e9703401476ac3636b187f44db0
AlexL <github@leual.ch>
2025-11-04 08:04:50 +0100

    alpha channel support

--- clipboard_paste.py
+++ clipboard_paste.py
@@ -5,7 +5,8 @@
 @description: Automatic paste the image from the clipboard
 """
 
-from torch import Tensor, from_numpy
+# from torch import Tensor, from_numpy
+import torch
 import numpy as np
 from PIL import ImageGrab, Image, UnidentifiedImageError
 from PIL.PngImagePlugin import PngImageFile
@@ -19,16 +20,27 @@ def GetPILImageFromClipboard() -> list[Image.Image] | None:
     clip: Image.Image | list[str] | None = ImageGrab.grabclipboard() # ImageGrab.grabclipboard()
     if clip is None:
         return None
+    
     elif isinstance(clip, list):
-        return [Image.open(fp=clip[i]) for i in range(len(clip))]
+        # r, g, b, a = img.split()
+        result = []
+        for i in clip:
+            img = Image.open(i)
+            img = img.convert(mode="RGBA" if len(img.split()) == 4 else "RGB")
+            result.append(img)
+        return result
+    
     elif isinstance(clip, Image.Image):
-        return [Image.frombytes(mode=clip.mode, size=clip.size, data=clip.tobytes()).convert(mode="RGB")]
+        print(f"{clip.mode=}")
+        im = Image.frombytes(mode=clip.mode, size=clip.size, data=clip.tobytes())
+        return [im.convert(mode="RGBA")]
+    
     else:
         return None
 
 
 class PasteImage():
-    RETURN_TYPES = ("IMAGE",)
+    RETURN_TYPES = IO.IMAGE, IO.MASK,
     FUNCTION = "paste"
     CATEGORY = "Hangover"
 
@@ -41,7 +53,8 @@ class PasteImage():
                 }
 
     @classmethod
-    def IS_CHANGED(cls, alt_image: Tensor | None = None) -> str:
+    def IS_CHANGED(cls, alt_image: torch.Tensor | None = None) -> str:
+        """This is nessesary for the change in the clipboard to be recognized by ConfyUI"""
         sha = sha256()
         img = GetPILImageFromClipboard()
         if img is None:
@@ -54,19 +67,25 @@ class PasteImage():
         return sha.digest().hex()
 
 
-    def paste(self, alt_image: Tensor | None = None) -> tuple[Tensor | None]:
-        image = GetPILImageFromClipboard()
-        if image is not None:
-            result = np.array(object=image).astype(dtype=np.float32) / 255.0
-            result = from_numpy(result)
+    def paste(self, alt_image: torch.Tensor | None = None) -> tuple[torch.Tensor | None, torch.Tensor | None]:
+        images = GetPILImageFromClipboard()
+        mask = None
+        if images is not None:
+            result: torch.Tensor | None = torch.from_numpy((np.array(images, dtype=np.float32) / 255.))
             print(f"Clipboard contains {result.shape[0]} image{'s' if result.shape[0]>1 else ''}: {result.shape=}")
+            if result.shape[3] == 4:
+                # looks like we have an alpha channel
+                mask = 1. - result[:, :, :, 3]
+                result = result[:, :, :, :3]
+            else:
+                mask = torch.zeros(size=(64,64), dtype=torch.float32, device="cpu").unsqueeze(dim=0)
         else:
             result = alt_image
 
         if result is None:
             raise UnidentifiedImageError("Error: No valid image found in the clipboard.")
 
-        return result,
+        return result, mask,
 
 
 def run_test():
@@ -80,10 +99,16 @@ def run_test():
             for p in pil:
                 p.show()
 
-        tensor = clp_paste.paste()[0]
-        if tensor is not None:
+        tensor, mask = clp_paste.paste()
+        if tensor is None:
+            print("No image")
+        else:
             print(f"{tensor.shape=}")
 
+        if mask is None:
+            print("no mask")
+        else:
+            print(f"{mask.shape=}")
 
     except UnidentifiedImageError:
         print("Clipboard does not contain image(s)")
