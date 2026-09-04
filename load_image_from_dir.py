d5050df4bd1680ad839c6d89a3ee5420473490b1
AlexL <github@leual.ch>
2026-09-04 21:16:29 +0200

    refactor

--- load_image_from_dir.py
+++ load_image_from_dir.py
@@ -6,17 +6,18 @@
 V3 node.
 """
 
-from typing import Any
-
+from typing import Callable
 from nodes import MAX_RESOLUTION
 import torch
 import numpy as np
 from pathlib import Path
-from PIL import Image, ImageSequence, ImageOps
+from PIL import Image, ImageSequence, ImageOps, UnidentifiedImageError
 from comfy_api.latest import io
 import node_helpers
 from hashlib import md5
 from _hashlib import HASH
+from comfy.model_management import processing_interrupted
+from comfy_api_nodes.util.common_exceptions import ProcessingInterrupted
 
 
 def load_image_with_mask(image_path: Path) -> tuple[torch.Tensor, torch.Tensor]:
@@ -38,12 +39,19 @@ def load_image_with_mask(image_path: Path) -> tuple[torch.Tensor, torch.Tensor]:
         frame = node_helpers.pillow(ImageOps.exif_transpose, frame)
         rgb = frame.convert("RGB")
 
-        if "A" in frame.getbands():
-            mask = np.array(frame.getchannel("A")).astype(np.float32) / 255.0
-            mask = 1.0 - torch.from_numpy(mask)  # invertiert: transparent → 1
+        if 'A' in frame.getbands():
+            # Alpha channel
+            mask = np.array(frame.getchannel('A')).astype(np.float32) / 255.0
+
+        elif 'P' in frame.mode and "transparency" in frame.info:
+            # Palette with transparency
+            mask = np.array(frame.convert(mode="RGBA").getchannel(channel='A')
+                ).astype(dtype=np.float32) / 255.0
+
         else:
-            w, h = frame.size
-            mask = torch.zeros((h, w), dtype=torch.float32)
+            mask = np.ones(frame.size, dtype=np.float32)
+
+        mask = 1.0 - torch.from_numpy(mask)
 
         image_np = np.array(rgb).astype(np.float32) / 255.0
         image_tensor = torch.from_numpy(image_np)[None,]  # [1, H, W, C]
@@ -62,19 +70,24 @@ class LoadImageIndexFromDir(io.ComfyNode):
     valid_extensions = {".png", ".jpg", ".jpeg", ".webp", ".avif"}
 
     @classmethod
-    def _list_images(cls, path: Path) -> tuple[list[Path], str]:
+    def _list_images(cls, path:Path, recursive:bool) -> tuple[list[Path], str]:
         """Returns a list of image files under *path* and the md5 hash."""
 
         results: list[Path] = []
-        hash = md5()
+        hash:HASH = md5()
 
         if path.is_file() and path.suffix.lower() in cls.valid_extensions:
             results = [path]
             hash.update(str(path).encode('utf8'))
 
         elif path.is_dir():
-            for p in path.iterdir():
-                if p.suffix.lower() in cls.valid_extensions:
+            glob:Callable = path.rglob if recursive else path.glob
+
+            for p in glob("*"):
+                if processing_interrupted():
+                    raise ProcessingInterrupted
+
+                if  p.is_file and p.suffix.lower() in cls.valid_extensions:
                     results.append(p)
                     hash.update(str(p).encode('utf8'))
 
@@ -92,22 +105,28 @@ class LoadImageIndexFromDir(io.ComfyNode):
             inputs=[
                 io.String.Input("dir"),
                 io.Int.Input("image_index", default=0, control_after_generate=io.ControlAfterGenerate.fixed),
+                io.Boolean.Input("recursive", tooltip="Recursive directory search"),
             ],
             outputs=[
                 io.Image.Output(display_name="image"),
                 io.Mask.Output(display_name="mask"),
                 io.Int.Output(display_name="index"),
+                io.String.Output(display_name="image_file")
             ],
         )
 
-    @classmethod
-    def fingerprint_inputs(cls, *, dir: str, image_index: int, **kwargs) -> str:
-        return cls._list_images(Path(dir))[1]
-
 
     @classmethod
-    def execute(cls, *, dir: str, image_index: int, **kwargs) -> io.NodeOutput:
-        images = cls._list_images(Path(dir))[0]
-        img_tensor, mask_tensor = load_image_with_mask(images[image_index])
+    def execute(cls, *, dir: str, image_index: int, recursive:bool, **kwargs) -> io.NodeOutput:
+        dir_path = Path(dir)
+        if not dir_path.exists():
+            raise FileNotFoundError(f"Directory does not exists: {dir}")
 
-        return io.NodeOutput(img_tensor, mask_tensor, image_index,)
+        image_list:list[Path] = cls._list_images(dir_path, recursive)[0]
+        try:
+            file = image_list[image_index]
+            img_tensor, mask_tensor = load_image_with_mask(file)
+        except IndexError as e:
+            raise ValueError("No more images!")
+
+        return io.NodeOutput(img_tensor, mask_tensor, image_index, str(file),)
